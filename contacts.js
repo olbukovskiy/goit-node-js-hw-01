50044b47bfb7ecffe1e6c0a4696690130a9c810a
Oleg Bukovskiy <olbukovskiy@gmail.com>
2023-02-23 22:11:57 +0200

    add functions logic

---
+++ contacts.js
@@ -0,0 +1,101 @@
+const fs = require("fs").promises;
+const path = require("path");
+require("colors");
+const generateUniqueId = require("generate-unique-id");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const contactsBuffer = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(contactsBuffer.toString());
+    return contacts;
+  } catch (error) {
+    console.log(error.message);
+    return error.message;
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contactsBuffer = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(contactsBuffer.toString());
+
+    const index = contacts.findIndex(
+      (contact) => contact.id === contactId.toString()
+    );
+
+    if (index === -1) throw new Error("Such contact was not found".red);
+
+    const searchContact = contacts[index];
+
+    return searchContact;
+  } catch (error) {
+    console.log(error.message);
+    return error.message;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contactsBuffer = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(contactsBuffer.toString());
+
+    const index = contacts.findIndex(
+      (contact) => contact.id === contactId.toString()
+    );
+
+    if (index === -1) throw new Error("Such contact was not found".red);
+
+    const filteredContacts = contacts.filter(
+      (contact) => contact.id !== contactId.toString()
+    );
+
+    await fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+    console.log(
+      `Contact with the id ${contactId} was successfully deleted!`.green
+    );
+  } catch (error) {
+    console.log(error.message);
+    return error.message;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contactsBuffer = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(contactsBuffer.toString());
+
+    const index = contacts.findIndex(
+      (contact) => contact.name === name.toString()
+    );
+
+    console.log(index);
+
+    if (index !== -1 || index === 0) {
+      throw new Error(
+        "Such contact is already present in your's contacts list!".red
+      );
+    }
+
+    const newContacts = [
+      ...contacts,
+      { id: generateUniqueId(), name, email, phone },
+    ];
+
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    console.log(
+      `${name} was successfully added to your's contacts list!`.green
+    );
+  } catch (error) {
+    console.log(error.message);
+    return error.message;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
